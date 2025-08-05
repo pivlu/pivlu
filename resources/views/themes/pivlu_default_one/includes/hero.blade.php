23b8132073541741c763932865399a133c785f85
Gabriel Chimilevschi <gabriel@clevada.com>
2025-08-05 16:55:28 +0300

    Update hero.blade.php

--- resources/views/themes/pivlu_default_one/includes/hero.blade.php
+++ resources/views/themes/pivlu_default_one/includes/hero.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
 
         <div class="site-heading">
-            <div class="title">Pivlu CMS - Clean Blog Theme</div>
+            <div class="title">Pivlu CMS - Default Theme</div>
             <div class="subheading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu mattis eros. Donec urna nulla, tristique et suscipit lacinia, dignissim sit amet lectus. Pellentesque quam arcu,
                 feugiat at tellus in, venenatis fermentum lectus</div>
         </div>
